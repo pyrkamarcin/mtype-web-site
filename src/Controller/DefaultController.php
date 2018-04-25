9f6be28709ac88d54b5683f3a4775a1d83336035
Marcin Pyrka <pyrka.marcin@gmail.com>
2018-04-25 14:38:44 +0200

    update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -18,9 +19,10 @@ class DefaultController extends Controller
      * @return Response
      * @Route(path="/",name="default_index")
      */
-    public function indexAction(): Response
+    public function indexAction(Request $request): Response
     {
 
+        dump($request);
         return 200;
         //return $this->render('default/index.html.twig');
     }
